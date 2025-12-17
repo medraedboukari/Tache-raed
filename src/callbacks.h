1a771f5fb692de6a4c430628910e89ce008d4abf
Mohamed Raed Boukari <mohamedraed.boukari@esprit.tn>
2025-12-17 15:09:57 +0100

    fix callbacks.h

--- src/callbacks.h
+++ src/callbacks.h
@@ -29,42 +29,12 @@ void on_treeviewmanage_row_activated(GtkTreeView *treeview,
 // REFRESH BUTTON
 void on_refresh_clicked(GtkButton *button, gpointer user_data);
 
+// AUTRES FONCTIONS (ajoutées correctement)
+void on_checkbutton_center_toggled(GtkToggleButton *togglebutton, gpointer user_data);
+void on_checkbutton_name_toggled(GtkToggleButton *togglebutton, gpointer user_data);
+void on_treeview_2_row_activated(GtkTreeView *treeview, GtkTreePath *path,
+                                 GtkTreeViewColumn *column, gpointer user_data);
+void on_refresh_2_clicked(GtkButton *button, gpointer user_data);
+void on_btchoose_clicked(GtkButton *button, gpointer user_data);
 
 #endif
-
-
-void
-on_treeview13_row_activated            (GtkTreeView     *treeview,
-                                        GtkTreePath     *path,
-                                        GtkTreeViewColumn *column,
-                                        gpointer         user_data);
-
-void
-on_button39_clicked                    (GtkButton       *button,
-                                        gpointer         user_data);
-
-void
-on_button40_clicked                    (GtkButton       *button,
-                                        gpointer         user_data);
-
-void
-on_checkbutton_center_toggled          (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_checkbutton_name_toggled            (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_treeview_2_row_activated            (GtkTreeView     *treeview,
-                                        GtkTreePath     *path,
-                                        GtkTreeViewColumn *column,
-                                        gpointer         user_data);
-
-void
-on_refresh_2_clicked                   (GtkButton       *button,
-                                        gpointer         user_data);
-
-void
-on_btchoose_clicked                    (GtkButton       *button,
-                                        gpointer         user_data);
